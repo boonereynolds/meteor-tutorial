4047d64ff3ef770eeca851c6634828f7ae91f03d
Boone Reynolds <boone.reynolds@gmail.com>
2017-04-06 11:14:38 -0700

    adds event handler to form submit

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -9,3 +9,26 @@ Template.body.helpers({
     return Tasks.find({})
   }
 })
+
+// 4.2
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault()
+
+
+    // Get value from form element
+    const target = event.target
+    const text = target.text.value
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    })
+
+    // Clear form
+    target.text.value = ''
+    console.log(event)
+  }
+})
