a2d6522390386cdc40eda5bd27997dd10a0b854a
Boone Reynolds <boone.reynolds@gmail.com>
2017-04-06 11:46:12 -0700

    checkbox to hide completed works

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,28 @@
 import { Template } from 'meteor/templating'
 
+// 7.3  Add state dictionary to the body
+import { ReactiveDict } from 'meteor/reactive-dict'
+
 import { Tasks } from '../api/tasks.js'
 
 // 5.4  Import Task component from the body
 import './task.js'
 import './body.html'
 
+// 7.3  Add state dictionary to the body
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+})
+
 Template.body.helpers({
   tasks() {
+    // 7.5  Add helpers to body template
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     // 4.3 Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } })
   }
@@ -33,5 +48,9 @@ Template.body.events({
     // Clear form
     target.text.value = ''
     console.log(event)
-  }
+  },
+  // 7.4  Add event handler for checkbox
+  'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
 })
